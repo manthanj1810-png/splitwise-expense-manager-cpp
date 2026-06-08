3bab35a050939fd6aeb97946041630fff5f90f5d
manthanj1810-png <234795965+manthanj1810-png@users.noreply.github.com>
2026-06-08 20:58:03 +0530

    Add Expense structure

---
+++ Include/Expense.h
@@ -0,0 +1,16 @@
+#ifndef EXPENSE_H
+#define EXPENSE_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Expense
+{
+    string payer;
+    double amount;
+    vector<string> participants;
+};
+
+#endif

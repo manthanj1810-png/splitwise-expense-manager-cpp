b24fe8408eb7031b6f98ddbd4ebde2379f54ae38
manthanj1810-png <234795965+manthanj1810-png@users.noreply.github.com>
2026-06-08 21:07:05 +0530

    Add menu-driven application

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include "../include/Splitwise.h"
+
 using namespace std;
 
-int main() {
-    cout << "Splitwise Expense Manager" << endl;
+int main()
+{
+    Splitwise app;
+
+    int choice;
+
+    do
+    {
+        cout << "\n===== Splitwise Expense Manager =====\n";
+
+        cout << "1. Add Expense\n";
+        cout << "2. View Expenses\n";
+        cout << "3. Exit\n";
+
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch(choice)
+        {
+            case 1:
+                app.addExpense();
+                break;
+
+            case 2:
+                app.showExpenses();
+                break;
+
+            case 3:
+                cout << "Goodbye!\n";
+                break;
+
+            default:
+                cout << "Invalid choice\n";
+        }
+
+    } while(choice != 3);
+
     return 0;
 }

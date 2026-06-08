28ce4ee8ea1e3f92b7846c2e8c1b11b181eccb3d
manthanj1810-png <234795965+manthanj1810-png@users.noreply.github.com>
2026-06-08 21:17:42 +0530

    Add ShowBalances function

--- Include/Splitwise.h
+++ Include/Splitwise.h
@@ -3,15 +3,18 @@
 
 #include <vector>
 #include "Expense.h"
+#include <unordered_map>
 
 class Splitwise
 {
 private:
     vector<Expense> expenses;
+    unordered_map<string,double> balances;
 
 public:
     void addExpense();
     void showExpenses();
+    void showBalances();
 };
 
 #endif
